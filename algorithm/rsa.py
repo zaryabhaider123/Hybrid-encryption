d7355e87642e1f0b4a6346a01d9be50e9f569062
Zaryab Haider <zaryab6018@gmail.com>
2026-09-01 19:34:13 -0400

    RSA error fixed and comments completed

--- algorithm/rsa.py
+++ algorithm/rsa.py
@@ -2,54 +2,61 @@
 
 from cryptography.hazmat.primitives.asymmetric import rsa, padding
 from cryptography.hazmat.primitives.asymmetric.padding import OAEP, MGF1
-# RSA key generatiion + OAEP padding for the encryption of EAS key
+# RSA key generation + OAEP padding for the encryption of AES key
 
 from cryptography.hazmat.primitives import serialization, hashes
-#serializaation for converting from to binary data and to save/load the PEM files
+#serialization: converts key objetcs from/to bytes, for saving/loading the PEM files
 #hashes needed by the OAEP for padding
 
 from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
 #AES for encrypting the main message
 
 from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC
-
-from cryptography.hazmat.primitives import keywrap
-# needed in encrypting symmetric key into Asymmetric key
-
+# for password based key derivation
 import os
-#to import built_in OS module
+#os.urandom() for generating secure random keys
 
 
 
 class RSAKeyManager:
 
-    def __init__(self, key_size=1028):
-        self.key_size = 1028
+    # Constructor for RSAKeyManager
+    #key_size = size of the key in bits (Recommended by NIST : 2048)
+    #public key and private key start as none until generate_key is called
+    def __init__(self, key_size=2048):
+        self.key_size = key_size
         self.public_key = None
         self.private_key = None
 
+    #Method for generating key pairs,
+    #the function "generate_private_key" is Crytography library fuction
+    #Public exponent = 65537 as default and recommended
     def generate_keys(self):
         self.private_key = rsa.generate_private_key(public_exponent=65537, key_size = self.key_size)
         self.public_key = self.private_key.public_key()
 
-    def serialize_private_key(self, password = none):
-        encryption = serialization.BestAvailableEncryption(password) if password else serialization.NoEncryption
+    # serialize the private key to PEM bytes, optionally password encrypted
+    def serialize_private_key(self, password = None):
+        encryption = serialization.BestAvailableEncryption(password) if password else serialization.NoEncryption()
         return self.private_key.private_bytes(
             encoding= serialization.Encoding.PEM,
             format=serialization.PrivateFormat.PKCS8,
             encryption_algorithm = encryption 
         )
 
+     # Method to  convert public key into bytes 
     def serialize_public_key(self):
          return self.public_key.public_bytes(
                     encoding= serialization.Encoding.PEM,
                     format=serialization.PublicFormat.SubjectPublicKeyInfo
                 )
 
-    def load_private_key(self, pem_data, password = none):
+    #This method loads the previously saved private key from PEM bytes
+    def load_private_key(self, pem_data, password = None):
         self.private_key = serialization.load_pem_private_key(pem_data, password)
         self.public_key = self.private_key.public_key()
 
+    #This method loads the previously saved public key from PEM bytes
     def load_public_key(self, pem_data):
         self.public_key = serialization.load_pem_public_key(pem_data)
 
